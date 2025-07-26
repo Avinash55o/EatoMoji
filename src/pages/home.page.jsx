37a73f194873887dc86b5889acb50473471fccab
Avinash Boruah <avinashboruah8@gmail.com>
2025-07-27 00:25:32 +0530

    file structure

--- src/pages/home.page.jsx
+++ src/pages/home.page.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import NavBar from '../components/navBar.component'
+import MoodCard from '../components/moodCard.component'
 export default function Home() {
   return (
     <div>
        <NavBar />
+       <MoodCard />
     </div>
   )
 }
