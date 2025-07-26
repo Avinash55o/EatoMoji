0d72948121cedb2a48b538b8a0c5416b10299759
Avinash Boruah <avinashboruah8@gmail.com>
2025-07-26 23:36:35 +0530

    structure setup and dependencies

---
+++ src/components/navBar.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div className='flex justify-center'>
+        <div className='w-[80%] h-5'>
+
+        </div>
+    </div>
+  )
+}
+
+export default NavBar
---
+++ src/pages/home.page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import NavBar from '../components/navBar.component'
+export default function Home() {
+  return (
+    <div>
+       <NavBar />
+    </div>
+  )
+}
