3b5920c2194f8db2962cdf7aee49939f4846a2d3
Avinash Boruah <avinashboruah8@gmail.com>
2025-07-27 16:37:52 +0530

    added font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        // Add your font families here, e.g. 'sans': ['Inter', 'sans-serif'],
+        fredoka: ['Fredoka', 'sans-serif'],
+        chewy: ['Chewy', 'sans-serif'],
+        comfortaa: ['Comfortaa', 'cursive'],
+      },
+    },
   },
   plugins: [
     function ({ addVariant }) {
       addVariant("aspect-ratio-supported", "@supports (aspect-ratio: 1 / 1)");
     },
   ],
-};
+}
