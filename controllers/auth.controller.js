0ae91fae3d77ebea34a642e580eac14181e71826
Avinash Boruah <avinashboruah8@gmail.com>
2025-08-15 16:25:18 +0530

    signup and signin

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,9 @@
 import sql from "../DB/index.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config()
 
 export const signup = async(req, res)=>{
   try {
@@ -8,6 +11,7 @@ export const signup = async(req, res)=>{
     // find all fields are given or not
     if (!name || !email || !password) {
       return res.status(400).json({message:"all fields are required"})}
+
     // check if the user already exists
     const existinguser= await sql`SELECT name FROM users WHERE email = ${email} LIMIT 1`
     if (existinguser) {
@@ -19,9 +23,38 @@ export const signup = async(req, res)=>{
 
     //store in the DB
     cosnt [user]= await sql`INSERT INTO user (email, password_hash) VALUES (${email},${hashPassword})`
-
     
+    //create jwt token
+    const token= jwt.sign({id: user.id, email: user.email}, process.env.JWT_SECRET,{expiresIn:'7d'})
+    
+    res.status(201).json({user,token, message:"signup successfull"})
 
+  } catch (error) {
+    res.status(500).json({error:"signup unsuccessfull"})
+  }
+}
+
+
+// sign-in controller
+export const signIn= async(req,res)=>{
+  try {
+    //destructuring
+    const {email,password}= req.body
+    //check all the fields are given or not
+    if(!email || !password){
+      return res.status(400).json({message:"ALL fields are required"})
+    }
+    //check if email is there or not
+    const user= await sql`SELECT id, email, password_hash FROM user WHERE email= ${email} LIMIT 1`
+    if(!user){
+      return res.status(401).json({message:"unauthorized! not able to find the user"})
+    }
+    //check if pass is correct or not
+    const correct_pass= await bcrypt.compare(password, user.password_hash)
+    if(!correct_pass){
+      return res.status(401).json({message:"password is incorrect"})
+    }
+    res.status(200).json({user:{id:user.id ,email: user.email},message:"successfully signin"})
   } catch (error) {
     
   }
