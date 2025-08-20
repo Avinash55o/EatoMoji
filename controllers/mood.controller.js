84c62876f6f4330365ffbb85e876a6d41a4a3be0
Avinash Boruah <avinashboruah8@gmail.com>
2025-08-20 23:15:49 +0530

    mood controller update

--- controllers/mood.controller.js
+++ controllers/mood.controller.js
@@ -1,8 +1,8 @@
 import sql from "../DB/index.js";
 
-export const lestMood= async (req, res)=>{
+export const listMood= async (req, res)=>{
   try {
-    const moodList= await sql`SELECT * FROM FOODS ORDER BY id`
+    const moodList= await sql`SELECT * FROM moods ORDER BY id`
     return res.json({message:"successfully get the mood list", moodList})
   } catch (error) {
     return res.status(500).json({message:"internal error"})
