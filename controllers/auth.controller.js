c41a6f38c1c52c51a4262d752e81940c8b5d76b3
Avinash Boruah <avinashboruah8@gmail.com>
2025-08-20 23:14:41 +0530

    auth update

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,5 @@
 import sql from "../DB/index.js";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 
@@ -13,8 +13,8 @@ export const signup = async(req, res)=>{
       return res.status(400).json({message:"all fields are required"})}
 
     // check if the user already exists
-    const existinguser= await sql`SELECT name FROM users WHERE email = ${email} LIMIT 1`
-    if (existinguser) {
+    const existinguser= await sql`SELECT name FROM users WHERE email= ${email} LIMIT 1`
+    if (existinguser.length > 0) {
       return res.status(409).json({message:"The user already exists"})
     }
 
@@ -22,16 +22,21 @@ export const signup = async(req, res)=>{
     const hashPassword= await bcrypt.hash(password,10);
 
     //store in the DB
-    cosnt [user]= await sql`INSERT INTO user (email, password_hash) VALUES (${email},${hashPassword})`
+    const [user]= await sql`INSERT INTO users (name ,email, password_hash) VALUES (${name},${email},${hashPassword}) RETURNING id, email`
     
     //create jwt token
     const token= jwt.sign({id: user.id, email: user.email}, process.env.JWT_SECRET,{expiresIn:'7d'})
-    
-    res.status(201).json({user,token, message:"signup successfull"})
+    console.log(token)
+    if(!token){
+      return res.status(500).json({message:"token generation failed"})
+    }
 
-  } catch (error) {
-    res.status(500).json({error:"signup unsuccessfull"})
-  }
+    res.status(201).json({token, message:"signup successfull"})
+
+    } catch (error) {
+      console.log('singup error:', error)
+      res.status(500).json({error:"signup unsuccessfull"})
+    }
 }
 
 
@@ -45,17 +50,20 @@ export const signIn= async(req,res)=>{
       return res.status(400).json({message:"ALL fields are required"})
     }
     //check if email is there or not
-    const user= await sql`SELECT id, email, password_hash FROM user WHERE email= ${email} LIMIT 1`
+    const [user]= await sql`SELECT id, email, password_hash, name FROM users WHERE email= ${email} LIMIT 1`
     if(!user){
       return res.status(401).json({message:"unauthorized! not able to find the user"})
     }
     //check if pass is correct or not
     const correct_pass= await bcrypt.compare(password, user.password_hash)
+    console.log(correct_pass)
     if(!correct_pass){
       return res.status(401).json({message:"password is incorrect"})
     }
-    res.status(200).json({user:{id:user.id ,email: user.email},message:"successfully signin"})
+    console.log(res.json())
+    res.status(200).json({user:{id:user.id ,email: user.email, name:user.name},message:"successfully signin"})
   } catch (error) {
-    
+    console.log('signin error:', error)
+    res.status(500).json({error:"signin failed"})
   }
 }
