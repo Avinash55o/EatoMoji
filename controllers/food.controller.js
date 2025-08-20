52e9664e597af8ea27446a714128057b040e953b
Avinash Boruah <avinashboruah8@gmail.com>
2025-08-20 23:15:24 +0530

    food controller update

--- controllers/food.controller.js
+++ controllers/food.controller.js
@@ -1,9 +1,9 @@
 import sql from "../DB/index.js";
-
+// get request dont required the req.body cause they don't have a body.
 export const listFood= async (req, res)=>{
   try {
-    const list= await sql`SELECT id, name, category, calories FROM FOODS ORDER BY id`
-    return res.json({message:"successfully get the food list", list})
+    const list= await sql`SELECT id, food_name, category, calories FROM foods ORDER BY id`
+    return res.status(200).json({message:"successfully get the food list", list})
   } catch (error) {
     return res.status(500).json({message:"internal error"})
   }
