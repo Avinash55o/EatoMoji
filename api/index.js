3c2a2174dd49cf9cb46f6d5393f1e0bc5fc1f43f
Avinash Boruah <avinashboruah8@gmail.com>
2025-08-20 23:16:52 +0530

    api added

---
+++ api/index.js
@@ -0,0 +1,21 @@
+import {Router} from 'express';
+import { signIn, signup } from '../controllers/auth.controller.js';
+import { listFood } from '../controllers/food.controller.js';
+import { listMood } from '../controllers/mood.controller.js';
+import { foodSuggestion } from '../controllers/suggestions.controller.js';
+
+const router=Router();
+
+//auth
+router.post('/auth/signup',signup)
+router.post('/auth/signin',signIn)
+
+//for me
+router.get('/moods', listMood)
+router.get('/foods', listFood)
+
+//suggestions
+router.get('/suggestions/:mood_id', foodSuggestion)
+
+
+export default router;
